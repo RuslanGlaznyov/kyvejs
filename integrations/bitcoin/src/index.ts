6bdfa36c786d2b51fe43cc87c651844151c678af
ruslanglaznyov <ruslan.glaznyov@gmail.com>
2022-12-11 18:04:41 +0200

    chore: ci/cd check

--- integrations/bitcoin/src/index.ts
+++ integrations/bitcoin/src/index.ts
@@ -3,5 +3,5 @@ import { Validator } from "@kyvejs/protocol";
 import Bitcoin from "./runtime";
 
 const runtime = new Bitcoin();
-// test block
+// test block check
 new Validator(runtime).bootstrap();
