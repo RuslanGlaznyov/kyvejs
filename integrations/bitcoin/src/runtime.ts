a29ce7400bef0d1c505e9c2ca12a9edf67dc82f7
ruslanglaznyov <ruslan.glaznyov@gmail.com>
2022-12-12 11:37:49 +0200

    chore: error

--- integrations/bitcoin/src/runtime.ts
+++ integrations/bitcoin/src/runtime.ts
@@ -7,7 +7,7 @@ export default class Bitcoin implements IRuntime {
   public name = name;
   public version = version;
 
-  async 1getDataItem(
+  async 12getDataItem(
     v: Validator,
     source: string,
     key: string
